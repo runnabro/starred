553ef87c3d82c6202372811353b6dc4da3c664fa
runnabro <tli415@gmail.com>
2019-12-30 15:35:31 -0800

    convert remaining pages

---
+++ pages/codesnippets/index.js
@@ -0,0 +1,103 @@
+import './../../css/index.scss'
+import Head from 'next/head';
+import Layout from '../../components/layout.js'
+
+export default function CodeSnippets() {
+  return (
+    <Layout>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <title>Hey, tell me about CodeSnippets.</title>
+      </Head>
+      <main className="grid-block col main">
+        <article className="article article-codesnippets">
+          <section className="grid-block col section padding-bottom-none hero hero-codesnippets">
+            <a className="back-arrow" href="/">
+              @@include('images/back-arrow.svg')
+            </a>
+            <div className="text-container">
+              <h1 className="h1">Code<wbr />Snippets</h1>
+              <p className="p">Environments to run and share your code with. Designed with Sass, Backbone.js, & Handlebars.</p>
+            </div>
+            <figure className="figure figure-xl img-wrapper img-codesnippets-hero">
+              <video className="video" src="/images/codesnippets/new-code-hero.mp4" autoplay loop muted playsinline height="483" width="800"></video>
+            </figure>
+          </section>
+          <section className="grid-block col section text-container padding-bottom-none padding-top-none">
+            <p className="p">When I joined the team, we were already building a platform for developers to run their code.</p>
+            <h2 className="h2">A New Homepage</h2>
+            <p className="p">Our homepage was focused on showing users what type of code they could run. After we built up regular returning user base, we wanted to incentivize users to continue to create new content. We wanted to build the smoothest path to creating content and showcase that content right on the homepage.</p>
+            <figure className="grid-block col align-center figure figure-oldsnippets">
+              <img className="img img-wrapper" src="/images/codesnippets/old-home.png" height="403" width="450" />
+              <figcaption className="figcaption">Our homepage when starting the project.</figcaption>
+            </figure>
+          </section>
+          <section className="grid-block section section-callout section-hero">
+            <div className="text-container">
+              <h2 className="h2">Creating Content</h2>
+              <p className="p">Rhe only path to creating a code example was with the “Create” button in the header. And users would see what languages were supported after starting the create flow.</p>
+              <p className="p">We put the languages we supported in the hero. Users could tap on the one they were interested in and start writing code right away.</p>
+            </div>
+            <img className="img no-shadow" src="/images/codesnippets/new-hero.png" height="444" width="550" />
+          </section>
+          <section className="grid-block col section padding-bottom-none">
+            <div className="text-container">
+              <h2 className="h2">Keeping Users Engaged</h2>
+              <p className="p">Code examples were not easy to find. We wanted code creators to have their content recognized, and our consumers to feel the code they were looking at was always revolving.</p>
+              <p className="p">We built a feed on the homepage to showcase new code and designed pages that were automatically generated for user selected tags.</p>
+            </div>
+            <figure className="grid-block col shrink figure figure-xl figure-nodejs">
+              <div className="img-wrapper img-nodejs">
+                <img className="img" src="/images/codesnippets/tag-page.png" height="484" width="760" />
+              </div>
+            </figure>
+          </section>
+          <section className="grid-block section padding-bottom-none section-callout section-profiles">
+            <div className="text-container">
+              <h2 className="h2">Creator Recognition</h2>
+              <div className="grid-block align-center">
+                <img className="img no-shadow img-profile-widget" src="/images/codesnippets/profile-small.png" height="70" width="200" />
+              </div>
+              <p className="p">To further encourage content creators, we added user profiles to give them visibility throughout CodeSnippets. Users were able to use their profile to track their current code snippets and drafts of their code before publishing.</p>
+            </div>
+            <div className="img-profile">
+              <img className="img no-shadow" src="/images/codesnippets/profile-big.png" height="467" width="300" />
+            </div>
+          </section>
+          <section className="grid-block section section-callout section-runoptions">
+            <img className="img no-shadow img-runoptions" src="/images/codesnippets/runoptions.gif" height="307" width="300" />
+            <div className="text-container">
+              <h2 className="h2">Build & Runtime</h2>
+              <p className="p">One of the unique features of CodeSnippets is that users have access to their environment through a web terminal. Later, we gave users more control of their environments by exposing the configuration of their environments.</p>
+            </div>
+          </section>
+          <section className="grid-block col section section-mobile">
+            <div className="text-container">
+              <h2 className="h2">Mobile Refactoring</h2>
+              <p className="p">When we started, CodeSnippets was not a responsive webapp even though user base was at ~30% mobile users (it’s even higher today). While working on the product, I gradually rewrote our front-end to be more semantic, accessible, and responsive.</p>
+            </div>
+            <figure className="grid-block figure figure-xl figure-mobile">
+              <img className="img no-shadow" src="/images/codesnippets/mobile/screenie-1.png" height="529" width="253" />
+              <img className="img no-shadow" src="/images/codesnippets/mobile/screenie-2.png" height="529" width="253" />
+              <img className="img no-shadow" src="/images/codesnippets/mobile/screenie-3.png" height="529" width="253" />
+            </figure>
+          </section>
+          <section className="grid-block col section padding-top-none">
+            <p className="text-container p">
+              Thanks for reading. Runnable has since been acquired. We’ve open sourced <a className="link" href="//github.com/codenow/runnable-web"><span className="span">our code</span></a> and CodeSnippets has been shutdown. 🙁
+            </p>
+            <div className="margin-top-md text-container">
+              <a className="btn btn-md btn-blue-outline" href="/">
+                @@include('images/back-arrow.svg')
+                Go Home
+              </a>
+            </div>
+          </section>
+        </article>
+      </main>
+    </Layout>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,9 @@ export default function Home() {
             <h2 className="h2 weight-bold">Notorious A.P.I.</h2>
             <p className="p">API response codes and latency represented in visual and audio form.</p>
             <div className="grid-block margin-top-md btn-wrapper">
-              <a className="btn btn-md btn-white" href="/notoriousapi/">Read Details</a>
+              <Link href="/notoriousapi">
+                <a className="btn btn-md btn-white">Read Details</a>
+              </Link>
               <a className="btn btn-md btn-white-outline" href="//mulesoft-labs.github.io/notoriousapi/"><span className="span">View It Live</span></a>
             </div>
           </section>
@@ -44,7 +46,9 @@ export default function Home() {
             <h2 className="h2 weight-bold">CodeSnippets</h2>
             <p className="p">Complete environments to run and share code in the browser.</p>
             <div className="grid-block margin-top-md btn-wrapper">
-              <a className="btn btn-md btn-white" href="/codesnippets/"><span className="span">Read Details</span></a>
+              <Link href="/codesnippets">
+                <a className="btn btn-md btn-white"><span className="span">Read Details</span></a>
+              </Link>
             </div>
             <img className="img no-shadow" src="/images/codesnippets.svg" alt="" />
           </section>
@@ -52,7 +56,9 @@ export default function Home() {
             <h2 className="h2 weight-bold">MuleQuest</h2>
             <p className="p">Introducing users to a new product with a game.</p>
             <div className="grid-block margin-top-md btn-wrapper">
-              <a className="btn btn-md btn-white" href="/mulequest/"><span className="span">Read Details</span></a>
+              <Link href="/mulequest">
+                <a className="btn btn-md btn-white"><span className="span">Read Details</span></a>
+              </Link>
             </div>
             <img className="img no-shadow" src="/images/mulequest.gif" alt="" />
           </section>
