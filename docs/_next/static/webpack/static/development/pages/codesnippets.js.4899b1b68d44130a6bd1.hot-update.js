654a4dc32fc357d486ccbfdf0c441a987fe442dc
runnabro <tli415@gmail.com>
2020-01-01 18:01:13 -0800

    use /docs for gh-pages

---
+++ docs/_next/static/webpack/static/development/pages/codesnippets.js.4899b1b68d44130a6bd1.hot-update.js
@@ -0,0 +1,490 @@
+webpackHotUpdate("static/development/pages/codesnippets.js",{
+
+/***/ "./pages/codesnippets/index.js":
+/*!*************************************!*\
+  !*** ./pages/codesnippets/index.js ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return CodeSnippets; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_BackLink_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../components/BackLink.js */ "./components/BackLink.js");
+/* harmony import */ var _components_HomeLink_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../components/HomeLink.js */ "./components/HomeLink.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! next/head */ "./node_modules/next/dist/next-server/lib/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _components_Layout_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../components/Layout.js */ "./components/Layout.js");
+var _jsxFileName = "/Users/tony/Documents/GitHub/starred/pages/codesnippets/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+function CodeSnippets() {
+  return __jsx(_components_Layout_js__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, __jsx(next_head__WEBPACK_IMPORTED_MODULE_3___default.a, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, __jsx("title", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "Hey, tell me about CodeSnippets.")), __jsx("main", {
+    className: "grid-block col main",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, __jsx("article", {
+    className: "article article-codesnippets",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, __jsx("section", {
+    className: "grid-block col section padding-bottom-none hero hero-codesnippets",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, __jsx(_components_BackLink_js__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }), __jsx("div", {
+    className: "text-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, __jsx("h1", {
+    className: "h1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "Code", __jsx("wbr", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }), "Snippets"), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, "Environments to run and share your code with. Designed with Sass, Backbone.js, & Handlebars.")), __jsx("figure", {
+    className: "figure figure-xl img-wrapper img-codesnippets-hero",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, __jsx("video", {
+    autoPlay: true,
+    className: "video",
+    height: "483",
+    loop: true,
+    muted: true,
+    playsInline: true,
+    src: "/images/codesnippets/new-code-hero.mp4",
+    width: "800",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }))), __jsx("section", {
+    className: "grid-block col section text-container padding-bottom-none padding-top-none",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }, "When I joined the team, we were already building a platform for developers to run their code."), __jsx("h2", {
+    className: "h2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, "A New Homepage"), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 36
+    },
+    __self: this
+  }, "Our homepage was focused on showing users what type of code they could run. After we built up regular returning user base, we wanted to incentivize users to continue to create new content. We wanted to build the smoothest path to creating content and showcase that content right on the homepage."), __jsx("figure", {
+    className: "grid-block col align-center figure figure-oldsnippets",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37
+    },
+    __self: this
+  }, __jsx("img", {
+    className: "img img-wrapper",
+    src: "/images/codesnippets/old-home.png",
+    height: "403",
+    width: "450",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 38
+    },
+    __self: this
+  }), __jsx("figcaption", {
+    className: "figcaption",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39
+    },
+    __self: this
+  }, "Our homepage when starting the project."))), __jsx("section", {
+    className: "grid-block section section-callout section-hero",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "text-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, __jsx("h2", {
+    className: "h2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 44
+    },
+    __self: this
+  }, "Creating Content"), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45
+    },
+    __self: this
+  }, "Rhe only path to creating a code example was with the \u201CCreate\u201D button in the header. And users would see what languages were supported after starting the create flow."), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 46
+    },
+    __self: this
+  }, "We put the languages we supported in the hero. Users could tap on the one they were interested in and start writing code right away.")), __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/codesnippets/new-hero.png",
+    height: "444",
+    width: "550",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  })), __jsx("section", {
+    className: "grid-block col section padding-bottom-none",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 50
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "text-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 51
+    },
+    __self: this
+  }, __jsx("h2", {
+    className: "h2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52
+    },
+    __self: this
+  }, "Keeping Users Engaged"), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 53
+    },
+    __self: this
+  }, "Code examples were not easy to find. We wanted code creators to have their content recognized, and our consumers to feel the code they were looking at was always revolving."), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 54
+    },
+    __self: this
+  }, "We built a feed on the homepage to showcase new code and designed pages that were automatically generated for user selected tags.")), __jsx("figure", {
+    className: "grid-block col shrink figure figure-xl figure-nodejs",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 56
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "img-wrapper img-nodejs",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 57
+    },
+    __self: this
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/codesnippets/tag-page.png",
+    height: "484",
+    width: "760",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  })))), __jsx("section", {
+    className: "grid-block section padding-bottom-none section-callout section-profiles",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 62
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "text-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  }, __jsx("h2", {
+    className: "h2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 64
+    },
+    __self: this
+  }, "Creator Recognition"), __jsx("div", {
+    className: "grid-block align-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 65
+    },
+    __self: this
+  }, __jsx("img", {
+    className: "img no-shadow img-profile-widget",
+    src: "/images/codesnippets/profile-small.png",
+    height: "70",
+    width: "200",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66
+    },
+    __self: this
+  })), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 68
+    },
+    __self: this
+  }, "To further encourage content creators, we added user profiles to give them visibility throughout CodeSnippets. Users were able to use their profile to track their current code snippets and drafts of their code before publishing.")), __jsx("div", {
+    className: "img-profile",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 70
+    },
+    __self: this
+  }, __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/codesnippets/profile-big.png",
+    height: "467",
+    width: "300",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 71
+    },
+    __self: this
+  }))), __jsx("section", {
+    className: "grid-block section section-callout section-runoptions",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 74
+    },
+    __self: this
+  }, __jsx("img", {
+    className: "img no-shadow img-runoptions",
+    src: "/images/codesnippets/runoptions.gif",
+    height: "307",
+    width: "300",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 75
+    },
+    __self: this
+  }), __jsx("div", {
+    className: "text-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 76
+    },
+    __self: this
+  }, __jsx("h2", {
+    className: "h2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 77
+    },
+    __self: this
+  }, "Build & Runtime"), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 78
+    },
+    __self: this
+  }, "One of the unique features of CodeSnippets is that users have access to their environment through a web terminal. Later, we gave users more control of their environments by exposing the configuration of their environments."))), __jsx("section", {
+    className: "grid-block col section section-mobile",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 81
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "text-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 82
+    },
+    __self: this
+  }, __jsx("h2", {
+    className: "h2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 83
+    },
+    __self: this
+  }, "Mobile Refactoring"), __jsx("p", {
+    className: "p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 84
+    },
+    __self: this
+  }, "When we started, CodeSnippets was not a responsive webapp even though user base was at ~30% mobile users (it\u2019s even higher today). While working on the product, I gradually rewrote our front-end to be more semantic, accessible, and responsive.")), __jsx("figure", {
+    className: "grid-block figure figure-xl figure-mobile",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 86
+    },
+    __self: this
+  }, __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/codesnippets/mobile/screenie-1.png",
+    height: "529",
+    width: "253",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 87
+    },
+    __self: this
+  }), __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/codesnippets/mobile/screenie-2.png",
+    height: "529",
+    width: "253",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 88
+    },
+    __self: this
+  }), __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/codesnippets/mobile/screenie-3.png",
+    height: "529",
+    width: "253",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 89
+    },
+    __self: this
+  }))), __jsx("section", {
+    className: "grid-block col section padding-top-none",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 92
+    },
+    __self: this
+  }, __jsx("p", {
+    className: "text-container p",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 93
+    },
+    __self: this
+  }, "Thanks for reading. Runnable has since been acquired. We\u2019ve open sourced ", __jsx("a", {
+    className: "link",
+    href: "//github.com/codenow/runnable-web",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 94
+    },
+    __self: this
+  }, __jsx("span", {
+    className: "span",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 94
+    },
+    __self: this
+  }, "our code")), " and CodeSnippets has been shutdown. \uD83D\uDE41"), __jsx("div", {
+    className: "margin-top-md text-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 96
+    },
+    __self: this
+  }, __jsx(_components_HomeLink_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 97
+    },
+    __self: this
+  }))))));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=codesnippets.js.4899b1b68d44130a6bd1.hot-update.js.map
