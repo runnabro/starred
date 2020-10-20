76dd66d2f6f620c25a410799dc4f3cd9ec0a48af
Tony Li <runnabro@users.noreply.github.com>
2020-10-20 14:39:48 -0700

    deploy

---
+++ docs/_next/static/1D8zE_EFA-UHKhc8bhRs0/pages/runnable.js
@@ -0,0 +1,298 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[20],{
+
+/***/ "76Fp":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Runnable; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("q1tI");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_BackLink_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("eiwE");
+/* harmony import */ var _components_HomeLink_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__("p5jH");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__("8Kt/");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _components_Layout_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__("5Yp1");
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+function Runnable() {
+  return __jsx(_components_Layout_js__WEBPACK_IMPORTED_MODULE_4__[/* default */ "a"], null, __jsx(next_head__WEBPACK_IMPORTED_MODULE_3___default.a, null, __jsx("title", null, "Hey, tell me about Runnable.")), __jsx("main", {
+    className: "grid-block col main"
+  }, __jsx("article", {
+    className: "article article-runnable"
+  }, __jsx("section", {
+    className: "section padding-bottom-none hero hero-runnable"
+  }, __jsx(_components_BackLink_js__WEBPACK_IMPORTED_MODULE_1__[/* default */ "a"], null), __jsx("div", {
+    className: "text-container"
+  }, __jsx("h1", {
+    className: "h1"
+  }, "Runnable"), __jsx("p", {
+    className: "p"
+  }, "Environments that fit your developer workflow. Featured on ", __jsx("a", {
+    className: "link",
+    href: "//nicelydone.club/products/runnable/"
+  }, __jsx("span", {
+    className: "span"
+  }, "Nicely done")), ". Designed with Sass, Angular, and Pug."), __jsx("a", {
+    className: "btn btn-md btn-white",
+    href: "//runnable.com/landing"
+  }, __jsx("span", {
+    className: "span"
+  }, "View It Live"))), __jsx("figure", {
+    className: "figure figure-xl img-wrapper img-marketing"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/howitworks.png",
+    height: "416",
+    width: "840"
+  }))), __jsx("section", {
+    className: "grid-block col section padding-top-none section-marketing"
+  }, __jsx("div", {
+    className: "text-container"
+  }, __jsx("h2", {
+    className: "h2"
+  }, "First Impressions"), __jsx("p", {
+    className: "p"
+  }, "Before getting to use the product, everyone starts on the marketing page. It\u2019s one of the best opportunities we have to convince you to give our product a shot. My team pushed me to develop my skills in copywriting, illustration, and Javascript. Our marketing pages were built similarly to our product and we made changes often to reflect the changes to our product.")), __jsx("figure", {
+    className: "grid-block align-center justify-center figures-marketing"
+  }, __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/runnable/marketing-icons.png",
+    height: "325",
+    width: "300"
+  }), __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/runnable/marketing-menu.png",
+    height: "285",
+    width: "300"
+  }))), __jsx("section", {
+    className: "section section-blog padding-top-none"
+  }, __jsx("figure", {
+    className: "grid-block justify-center figure figure-xl figure-blog"
+  }, __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/runnable/blog-1.png",
+    height: "254",
+    width: "200"
+  }), __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/runnable/blog-email.png",
+    height: "522",
+    width: "250"
+  }), __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/runnable/blog-2.png",
+    height: "254",
+    width: "200"
+  })), __jsx("div", {
+    className: "text-container"
+  }, __jsx("h2", {
+    className: "h2"
+  }, "The Runnable Blog"), __jsx("p", {
+    className: "p"
+  }, "Building ", __jsx("a", {
+    className: "link",
+    href: "//runnable.com/blog"
+  }, __jsx("span", {
+    className: "span"
+  }, "our blog")), " was a new challenge for me. Working with our engineers to write posts helped me realize how important writing is in the products we design. When looking at the tone of what we\u2019ve written to users, I\u2019ve learned to ask myself how our users are feeling; \u201CAre you eager to get started now, or are you feeling frustrated?\u201D."))), __jsx("section", {
+    className: "grid-block col section padding-bottom-none"
+  }, __jsx("div", {
+    className: "text-container"
+  }, __jsx("h2", {
+    className: "h2"
+  }, "An Easier Way to Containerize Apps"), __jsx("p", {
+    className: "p"
+  }, "We needed users to use Docker, but when we were getting started we found that most everyone was new to writing Dockerfiles. Every application needed a Dockerfile that defined the configuration for their environment. In one of our earliest versions of our set up flow, we exposed the Dockerfile directly to users and provided basic syntax validation.")), __jsx("figure", {
+    className: "grid-block col shrink figure figure-xl"
+  }, __jsx("div", {
+    className: "img-wrapper img-simple-mode"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/simple-configuration-v1.png",
+    width: "500"
+  }))), __jsx("div", {
+    className: "text-container"
+  }, __jsx("p", {
+    className: "p"
+  }, "Basic validation was not enough for users to make it to running their projects. We learned that our most successful users were doing well when they had working Dockerfiles that our team was able to build with them.")), __jsx("figure", {
+    className: "grid-block col shrink figure figure-xl"
+  }, __jsx("div", {
+    className: "img-wrapper img-simple-mode"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/simple-configuration-v2.png",
+    width: "500"
+  }))), __jsx("div", {
+    className: "text-container"
+  }, __jsx("p", {
+    className: "p"
+  }, "We took the knowledge that our team learned from helping other orgs make Dockerfiles to build a guided flow. Users would be able to describe their application to us, so we could generate a Dockerfile for them.")), __jsx("figure", {
+    className: "grid-block col shrink figure figure-xl"
+  }, __jsx("div", {
+    className: "img-wrapper img-simple-mode"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/simple-configuration.png",
+    height: "461",
+    width: "800"
+  })))), __jsx("section", {
+    className: "grid-block align-center section section-slash-docker padding-bottom-none padding-top-none"
+  }, __jsx("figure", {
+    className: "grid-block shrink col align-center figure img-wrapper"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/slash-docker.png",
+    width: "250"
+  })), __jsx("div", {
+    className: "grid-block col text-container"
+  }, __jsx("h2", {
+    className: "h2"
+  }, "/docker"), __jsx("p", {
+    className: "p"
+  }, "We started a ", __jsx("a", {
+    className: "link",
+    href: "//runnable.com/docker/"
+  }, __jsx("span", {
+    className: "span"
+  }, "Docker resource")), " to help developers new to Docker. I worked with one of our PMs to design and build the site for the articles we were planning. We choose to use Jekyll because we needed an easy way to write and edit articles in Markdown; and it worked out so well, we ended up forking the project for our ", __jsx("a", {
+    className: "link",
+    href: "//runnable.com/docs"
+  }, __jsx("span", {
+    className: "span"
+  }, "docs")), " and ", __jsx("a", {
+    className: "link",
+    href: "//runnable.com/blog"
+  }, __jsx("span", {
+    className: "span"
+  }, "blog")), "."))), __jsx("section", {
+    className: "grid-block section section-callout section-dockerfile"
+  }, __jsx("div", {
+    className: "text-container"
+  }, __jsx("h2", {
+    className: "h2"
+  }, "The Dockerfile"), __jsx("p", {
+    className: "p"
+  }, "Users could manage their Dockerfile in their repository and we would relaunch environments when they made changes on GitHub.")), __jsx("div", {
+    className: "img-dockerfile"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/dockerfile.png",
+    height: "485",
+    width: "880"
+  }))), __jsx("section", {
+    className: "grid-block align-center col section section-branches"
+  }, __jsx("div", {
+    className: "grid-block align-center section-callout"
+  }, __jsx("div", {
+    className: "grid-block col text-container"
+  }, __jsx("h2", {
+    className: "h2"
+  }, "Workflow Oriented Design"), __jsx("p", {
+    className: "p"
+  }, "Most continuous development tools presented code by build number, but that made it hard to identify what you\u2019re running or testing. We choose to build code by branches, mirroring our user\u2019s existing workflow.")), __jsx("figure", {
+    className: "grid-block shrink col align-center figure"
+  }, __jsx("img", {
+    className: "img img-branches no-shadow",
+    src: "/images/runnable/branch-environments.png",
+    height: "209",
+    width: "300"
+  }), __jsx("figcaption", {
+    className: "figcaption"
+  }, "Onboarding users to branch based environments."))), __jsx("figure", {
+    className: "grid-block shrink col align-center figure"
+  }, __jsx("img", {
+    className: "img no-shadow",
+    src: "/images/runnable/onboarding.png",
+    width: "330"
+  })), __jsx("div", {
+    className: "text-container"
+  }, __jsx("p", {
+    className: "p"
+  }, "We extended our user philosophy to our onboarding strategy by using real GitHub projects to demo the value we add to developers."))), __jsx("section", {
+    className: "grid-block col section padding-bottom-none section-integrations"
+  }, __jsx("div", {
+    className: "text-container"
+  }, __jsx("h2", {
+    className: "h2"
+  }, "Deeper Workflow Integration"), __jsx("p", {
+    className: "p"
+  }, "We wanted to bring environments to the tools teams already use. If users choose to manage their Dockerfile in their repo, project collaborators could use Runnable without ever using our app. Those same environments can be exposed to PMs and QA teams in Jira and Slack integrations too.")), __jsx("div", {
+    className: "grid-block justify-center figures"
+  }, __jsx("figure", {
+    className: "grid-block col shrink figure"
+  }, __jsx("div", {
+    className: "img-wrapper img-pr"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/github-integration.png",
+    height: "280",
+    width: "520"
+  })), __jsx("figcaption", {
+    className: "figcaption"
+  }, "\u201CRunnabot\u201D making it easy for developers to access their environments.")), __jsx("figure", {
+    className: "grid-block col shrink figure"
+  }, __jsx("div", {
+    className: "img-wrapper img-jira"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/jira-integration.png",
+    height: "273",
+    width: "300"
+  })), __jsx("figcaption", {
+    className: "figcaption"
+  }, "A test of our Jira integration."))), __jsx("p", {
+    className: "text-container p"
+  }, "If your tests failed or something went wrong you can access logs and options to manage or configure your environments in Runnable."), __jsx("figure", {
+    className: "grid-block col shrink figure figure-xxl figure-app"
+  }, __jsx("div", {
+    className: "img-wrapper img-app"
+  }, __jsx("img", {
+    className: "img",
+    src: "/images/runnable/in-app-view.png",
+    height: "512",
+    width: "920"
+  })))), __jsx("section", {
+    className: "grid-block col section margin-top-none"
+  }, __jsx("p", {
+    className: "text-container p"
+  }, "Thanks for reading! Runnable has since ", __jsx("a", {
+    className: "link",
+    href: "//runnable.com"
+  }, __jsx("span", {
+    className: "span"
+  }, "been acquired")), ", so we were able to make the source for our ", __jsx("a", {
+    className: "link",
+    href: "//github.com/codenow/runnable.com"
+  }, __jsx("span", {
+    className: "span"
+  }, "marketing pages")), " and ", __jsx("a", {
+    className: "link",
+    href: "//github.com/codenow/runnable-angular"
+  }, __jsx("span", {
+    className: "span"
+  }, "product")), " public."), __jsx("div", {
+    className: "margin-top-md text-container"
+  }, __jsx(_components_HomeLink_js__WEBPACK_IMPORTED_MODULE_2__[/* default */ "a"], null))))));
+}
+
+/***/ }),
+
+/***/ "sTJY":
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/runnable", function() {
+      var mod = __webpack_require__("76Fp")
+      if(false) {}
+      return mod
+    }]);
+  
+
+/***/ })
+
+},[["sTJY",0,1]]]);
