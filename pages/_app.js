fc2fa21f7c0a3566639dfacf48520b14fe2ef041
Tony Li <runnabro@users.noreply.github.com>
2022-12-25 16:55:43 -0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/index.scss'
+import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,112 +1,37 @@
-import React from 'react';
 import Head from 'next/head'
-import Layout from '../components/Layout.js'
-import Link from 'next/link'
-import Project from '../components/Project.js'
-import SubProject from '../components/SubProject.js'
+import Image from 'next/image'
+import localFont from '@next/font/local'
+import styles from '../styles/Home.module.scss'
 
-export default class extends React.Component {
-  renderProjects = () => {
-    return (
-      <div className="grid-block section-card-wrapper">
-        <section className="section-card section-runnable">
-          <Project
-            description="Automatically launching full-stack environments for every branch."
-            detailsHref="/runnable"
-            liveHref="//runnable.com/landing"
-            imgShadow
-            imgSrc="/images/runnable/dockerfile.png"
-            title="Runnable"
-          />
-        </section>
-        <section className="section-card section-notorious">
-          <Project
-            description="API response codes and latency represented in visual and audio form."
-            detailsHref="/notoriousapi"
-            liveHref="//mulesoft-labs.github.io/notoriousapi/"
-            title="Notorious A.P.I."
-          />
-        </section>
-        <section className="section-card section-codesnippets">
-          <Project
-            description="Complete environments to run and share code in the browser."
-            detailsHref="/codesnippets"
-            imgSrc="/images/codesnippets.svg"
-            title="CodeSnippets"
-          />
-        </section>
-        <section className="section-card section-mulequest">
-          <Project
-            description="Introducing users to a new product with a game."
-            detailsHref="/mulequest"
-            imgSrc="/images/mulequest.gif"
-            title="MuleQuest"
-          />
-        </section>
-      </div>
-    )
-  };
+const garamond = localFont({
+  src: [{
+    path: '../fonts/Garamond.ttf',
+    style: 'normal',
+    weight: '400',
+  },
+  {
+    path: '../fonts/Garamond-Bold.ttf',
+    style: 'normal',
+    weight: '700',
+  }],
+  variable: '--garamond',
+});
 
-  renderSubProjects = () => {
-    return (
-      <section className="section padding-top-none section-more">
-        <ol className="grid-block list section-card-wrapper">
-          <li className="grid-block col section-card li">
-            <SubProject
-              description="A resource for developers learning Docker and containerization."
-              imgSrc="/images/slash-docker.svg"
-              sourceHref="//github.com/runnable/slash-docker"
-              liveHref="//runnable.com/docker/"
-              title="/docker"
-            />
-          </li>
-          <li className="grid-block col section-card li">
-            <SubProject
-              description="Documentation for getting start with, and troubleshooting Runnable."
-              imgSrc="/images/file-docs.svg"
-              sourceHref="//github.com/runnable/docs"
-              liveHref="//runnable.com/docs/"
-              title="Runnable Docs"
-            />
-          </li>
-          <li className="grid-block col section-card li">
-            <SubProject
-              description="A simple way to calculate the CO² cost of a flight."
-              imgSrc="/images/co2flights.png"
-              sourceHref="//github.com/runnabro/offset"
-              liveHref="//ohgodhelp.us/"
-              title="CO² Flight Calculator"
-            />
-          </li>
-        </ol>
-      </section>
-    )
-  };
-
-  render() {
-    return (
-      <Layout>
-        <Head>
-          <title>Hi.</title>
-        </Head>
-        <main className="grid-block col main main-landing">
-          <section className="grid-block justify-center section section-intro">
-            <div className="text-container">
-              <h1 className="h1 weight-bold">Heya, I’m Tony.</h1>
-              <h1 className="h1 weight-bold">I work on <strong className="the-web"><span className="span" data-text="the web">the web</span></strong> & here’s some of what I’ve been up to:</h1>
-            </div>
-          </section>
-          {this.renderProjects()}
-          {this.renderSubProjects()}
-          <footer className="grid-block justify-center footer">
-            <a className="link-icon" href="//github.com/runnabro/">
-              <svg className="svg">
-                <use href="#github" />
-              </svg>
-            </a>
-          </footer>
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>🖖 Hi.</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className={garamond.variable}>
+        <main className={styles.main}>
+          <h1 className={styles.h1}>
+            I’m Tony. I do work on the <span className={styles['web']}>web</span> & this is what I’ve done.
+          </h1>
         </main>
-      </Layout>
-    )
-  }
+      </body>
+    </>
+  )
 }
